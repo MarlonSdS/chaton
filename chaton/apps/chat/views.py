a485a4fd39c6fe3696ae6fecf02055f06f902386
Marlon dos Santos <marlondossantos03@outlook.com>
2024-09-04 20:17:50 -0300

    início da integração com o frontend

--- chaton/apps/chat/views.py
+++ chaton/apps/chat/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Message, Room
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Esse é o chaton")
+    messages = Message.objects.all()
+    rooms = Room.objects.all()
+    return render(request, 'chat/index.html', {
+        'messages':messages,
+        'rooms': rooms})
